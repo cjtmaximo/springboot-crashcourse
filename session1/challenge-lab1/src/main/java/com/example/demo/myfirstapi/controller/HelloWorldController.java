586c869e86909e4c0ff5214feab0b61e01e6cef2
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-07-16 14:58:20 +0800

    refactor: add subfolder for challenge lab 1

--- session1/challenge-lab1/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ session1/challenge-lab1/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -1,14 +1,18 @@
 package com.example.demo.myfirstapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    @Value("${app.greeting.message}")
+    private String customGreeting;
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Spring Boot Learners!";
+        return customGreeting;
     }
 
     @GetMapping("/greet/{name}")
